33f467027696f347c7f8ca1a0ba68fff8f6e4269
Erwin Elling <erwinelling@gmail.com>
2014-06-22 23:52:20 +0200

    Skip logout confirmation

--- babybool/urls.py
+++ babybool/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'babybool.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     (r'^', include('babypage.urls')),   
     url(r'^accounts/', include('allauth.urls')),
     url(r'^admin/', include(admin.site.urls)),
